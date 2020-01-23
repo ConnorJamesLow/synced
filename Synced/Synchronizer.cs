96003c3e0e8faa7b989577c19728b67afaa41518
Connor Low <clow2@my.gcu.edu>
2020-01-22 19:29:47 -0800

    init dotnet project.

---
+++ Synced/Synchronizer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Synced
+{
+    public class Synchronizer
+    {
+        public static void SyncTable<T>()
+        {
+            
+        }
+    }
+}
