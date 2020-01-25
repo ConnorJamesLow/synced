c273248481dc2e0092de884855b0c02723c565e5
Connor Low <clow2@my.gcu.edu>
2020-01-24 21:19:01 -0800

    Divided Synchronizer class and added checks for existing Table

--- Synced/Synchronizer.cs
+++ Synced/Synchronizer.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Reflection;
+using Microsoft.Data.SqlClient;
 using Synced.Attributes;
 
 namespace Synced
 {
-    public class Synchronizer
+    public partial class Synchronizer
     {
         private readonly string _connection;
         public Synchronizer(string connectionString)
@@ -18,6 +18,20 @@ namespace Synced
         public void SyncTable<T>()
         {
             Type type = typeof(T);
+            string tableName = type.FullName;
+            if (TableExists(tableName))
+            {
+                if(TableHasRows(tableName))
+                {
+                    // TODO Update table
+                    return;
+                }
+            }
+            CreateTable(type);
+        }
+
+        public void CreateTable(Type type)
+        {
             if (type.FullName.StartsWith("System"))
             {
                 throw new TypeAccessException($"{type.FullName} is not permitted as a Table Model");
@@ -89,91 +103,8 @@ CREATE TABLE {schema.Name} ({columnsSql.ToString().Substring(1)}
     ";
 
             // Connect to the database and create the table
-            SqlConnection connection = new SqlConnection(_connection);
-            try
-            {
-                connection.Open();
-                SqlCommand command = new SqlCommand(sql, connection);
-                command.ExecuteNonQuery();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-            finally
-            {
-                connection.Close();
-            }
+            SqlCommand command = CreateCommand(sql);
+            ExecuteCommand(command);
         }
-
-        /// <summary>
-        /// Gets the best guess <see cref="ColumnType"/> related to a property.
-        /// </summary>
-        /// <param name="property"></param>
-        /// <returns></returns>
-        public ColumnType GetColumnType(PropertyInfo property)
-        {
-            DataType attribute = property.GetCustomAttribute<DataType>();
-            if (attribute != null)
-            {
-                return attribute.ColumnType;
-            }
-            else if (DefaultTypeMapping.ContainsKey(property.PropertyType))
-            {
-                return DefaultTypeMapping[property.PropertyType];
-            }
-            return ColumnType.varchar;
-        }
-
-        public int[] GetSizeModifier(PropertyInfo property, int parameterCount = 0)
-        {
-            if (parameterCount == 0)
-            {
-                return new int[] { };
-            }
-
-            // First, try getting the value from DataSize.Size
-            DataSize sizeAttribute = property.GetCustomAttribute<DataSize>();
-            if (sizeAttribute != null)
-            {
-                if (parameterCount == 2)
-                {
-                    return new int[] { sizeAttribute.Param1, sizeAttribute.Param2 };
-                }
-                return new int[] { sizeAttribute.Param1 };
-            }
-
-            // Next, try getting the value from the DataType.Size
-            DataType typeAttribute = property.GetCustomAttribute<DataType>();
-            if (typeAttribute != null)
-            {
-                if (parameterCount == 2)
-                {
-                    return new int[] { typeAttribute.SizeModifier, typeAttribute.SizeModifier2 };
-                }
-                return new int[] { typeAttribute.SizeModifier };
-            }
-            return new int[] { 0 };
-        }
-
-        /// <summary>
-        /// Provides some default mappings between C# data types and T-SQL data types.
-        /// </summary>
-        public Dictionary<Type, ColumnType> DefaultTypeMapping = new Dictionary<Type, ColumnType>
-        {
-            { typeof(bool), ColumnType.bit },
-            { typeof(int), ColumnType.@int },
-            { typeof(uint), ColumnType.@int },
-            { typeof(short), ColumnType.smallint },
-            { typeof(ushort), ColumnType.smallint },
-            { typeof(long), ColumnType.bigint },
-            { typeof(ulong), ColumnType.bigint },
-            { typeof(double), ColumnType.numeric },
-            { typeof(float), ColumnType.@float },
-            { typeof(byte), ColumnType.tinyint },
-            { typeof(string), ColumnType.varchar },
-            { typeof(IEnumerable<byte>), ColumnType.binary },
-            { typeof(DateTime), ColumnType.datetime }
-        };
     }
 }
---
+++ Synced/Synchronizer_Utils.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Data.SqlClient;
+using Synced.Attributes;
+
+namespace Synced
+{
+    public partial class Synchronizer
+    {
+        public bool TableExists(string tableName)
+        {
+            string sql = @"IF (EXISTS (
+	SELECT * 
+	FROM INFORMATION_SCHEMA.TABLES
+	WHERE TABLE_NAME = @Table
+))
+SELECT 1 AS [Exists]
+ELSE
+SELECT 0 AS [Exists]";
+            SqlCommand command = CreateCommand(sql);
+            command.Parameters.AddWithValue("@Table", tableName);
+            return ExecuteSingleReturn<int>(command) == 1;
+        }
+
+        public bool TableHasRows(string tableName)
+        {
+            string sql = @$"SELECT TOP 1 COUNT(*) FROM {tableName}";
+            SqlCommand command = CreateCommand(sql);
+            command.Parameters.AddWithValue("@Table", tableName);
+            return ExecuteSingleReturn<int>(command) > 0;
+        }
+
+        public SqlCommand CreateCommand(string sql) => new SqlCommand(sql);
+
+        public void ExecuteCommand(SqlCommand command)
+        {
+            command.Connection = new SqlConnection(_connection);
+            command.Connection.Open();
+            command.ExecuteNonQuery();
+            command.Connection.Close();
+        }
+
+        public T ExecuteSingleReturn<T>(SqlCommand command)
+        {
+            T result;
+            try
+            {
+                command.Connection = new SqlConnection(_connection);
+                command.Connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (!reader.Read())
+                {
+                    return default;
+                }
+                result = (T)reader[0];
+            }
+            finally
+            {
+                command.Connection.Close();
+            }
+            return result;
+        }
+
+
+
+        /// <summary>
+        /// Gets the best guess <see cref="ColumnType"/> related to a property.
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        public ColumnType GetColumnType(PropertyInfo property)
+        {
+            DataType attribute = property.GetCustomAttribute<DataType>();
+            if (attribute != null)
+            {
+                return attribute.ColumnType;
+            }
+            else if (DefaultTypeMapping.ContainsKey(property.PropertyType))
+            {
+                return DefaultTypeMapping[property.PropertyType];
+            }
+            return ColumnType.varchar;
+        }
+
+        public int[] GetSizeModifier(PropertyInfo property, int parameterCount = 0)
+        {
+            if (parameterCount == 0)
+            {
+                return new int[] { };
+            }
+
+            // First, try getting the value from DataSize.Size
+            DataSize sizeAttribute = property.GetCustomAttribute<DataSize>();
+            if (sizeAttribute != null)
+            {
+                if (parameterCount == 2)
+                {
+                    return new int[] { sizeAttribute.Param1, sizeAttribute.Param2 };
+                }
+                return new int[] { sizeAttribute.Param1 };
+            }
+
+            // Next, try getting the value from the DataType.Size
+            DataType typeAttribute = property.GetCustomAttribute<DataType>();
+            if (typeAttribute != null)
+            {
+                if (parameterCount == 2)
+                {
+                    return new int[] { typeAttribute.SizeModifier, typeAttribute.SizeModifier2 };
+                }
+                return new int[] { typeAttribute.SizeModifier };
+            }
+            return new int[] { 0 };
+        }
+
+        /// <summary>
+        /// Provides some default mappings between C# data types and T-SQL data types.
+        /// </summary>
+        public Dictionary<Type, ColumnType> DefaultTypeMapping = new Dictionary<Type, ColumnType>
+        {
+            { typeof(bool), ColumnType.bit },
+            { typeof(int), ColumnType.@int },
+            { typeof(uint), ColumnType.@int },
+            { typeof(short), ColumnType.smallint },
+            { typeof(ushort), ColumnType.smallint },
+            { typeof(long), ColumnType.bigint },
+            { typeof(ulong), ColumnType.bigint },
+            { typeof(double), ColumnType.numeric },
+            { typeof(float), ColumnType.@float },
+            { typeof(byte), ColumnType.tinyint },
+            { typeof(string), ColumnType.varchar },
+            { typeof(IEnumerable<byte>), ColumnType.binary },
+            { typeof(DateTime), ColumnType.datetime }
+        };
+    }
+}
